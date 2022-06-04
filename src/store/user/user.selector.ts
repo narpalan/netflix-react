64093c5a2e1864257f52eab613761d92729b912b
palannar <thiagofumega@gmail.com>
2022-06-03 22:49:19 -0300

    add readme

--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -1,1 +1,21 @@
+//import { createSelector } from "@reduxjs/toolkit";
+
 export const authenticated = (state:any) => state.user.authenticated;
+
+/*
+export const authenticated =  (state:any) => {
+    //altera a referencia de state.user.authenticated   
+    const isLogged = !state.user.authenticated
+    return isLogged
+}
+*/
+
+/* Exemplo de diferente estilo de memorização/referenciação
+export const products = (state:any)=> state.cart.products
+export const listProducts = createSelector(
+    [products],
+    (p:string[]) => {
+        p.map(name=>({name}))
+    }
+)
+*/
